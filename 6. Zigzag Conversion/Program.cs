66a7671632d995f68374e0ea40a01e86144631f4
Demine Nguyen <cuonghustmi1@gmail.com>
2023-09-30 19:35:51 +0700

    Demine
    30/09/2023

--- 6. Zigzag Conversion/Program.cs
+++ 6. Zigzag Conversion/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: s = "PAYPALISHIRING", numRows = 3
+    Output: "PAHNAPLSIIGYIR"
+
+ * Example 2:
+    Input: s = "PAYPALISHIRING", numRows = 4
+    Output: "PINALSIGYAHRPI"
+    Explanation:
+    P     I    N
+    A   L S  I G
+    Y A   H R
+    P     I
+
+ * Example 3:
+    Input: s = "A", numRows = 1
+    Output: "A"
+ */
