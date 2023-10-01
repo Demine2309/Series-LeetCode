4ff7d3d0601bcd72e517159b646bd8bcebab0e8b
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-01 10:31:12 +0700

    Demine
    1/10/2023

---
+++ 7. Reverse Integer/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: x = 123
+    Output: 321
+
+ * Example 2:
+    Input: x = -123
+    Output: -321
+
+ * Example 3:
+    Input: x = 120
+    Output: 21
+ */
