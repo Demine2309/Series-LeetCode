990983039639a60f8d45d39a79b779956afc2903
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-05 20:56:07 +0700

    Demine
    5/10/2023

--- 10. Regular Expression Matching/Program.cs
+++ 10. Regular Expression Matching/Program.cs
@@ -4,7 +4,44 @@
     {
         public bool IsMatch(string s, string p)
         {
+            int m = s.Length;
+            int n = p.Length;
 
+            // Create a matrix to store matching results
+            bool[,] dp = new bool[m + 1, n + 1];
+
+            // An empty string matches an empty pattern
+            dp[0, 0] = true;
+
+            // Fill the first row of the matrix
+            for (int j = 1; j <= n; j++)
+            {
+                if (p[j - 1] == '*')
+                    // If the current character in the pattern is '*',
+                    // We can skip it and its preceding character.
+                    dp[0, j] = dp[0, j - 2];
+            }
+
+            // Fill the matrix
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (s[i - 1] == p[j - 1] || p[j - 1] == '.')
+                        // If the current characters match or if the pattern has a '.', they match.
+                        dp[i, j] = dp[i - 1, j - 1];
+                    else if (p[j - 1] == '*')
+                        // If the current character in the pattern is '*'
+                        // 1. We can choose to skip the preceding character (0 occurrences of it), so dp[i, j] depends on dp[i, j - 2]
+                        // 2. We can choose to use the preceding character (1 or more occurrences of it), so dp[i, j] depends on dp[i - 1, j]
+                        //    Additionally, if the preceding character matches the current character in the input string or if the precedingg character
+                        //    in the pattern is '.', then dp[i, j] is also true.
+                        dp[i, j] = dp[i, j - 2] || (dp[i - 1, j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+                }
+            }
+
+            // The final result is stored in dp[m, n], which represents whether the entire input string matches the entire pattern.
+            return dp[m, n];
         }
     }
 
@@ -12,9 +49,14 @@
     {
         static void Main(string[] args)
         {
-            Solution solution = new Solution(); 
+            Solution solution = new Solution();
 
+            string s = "aa";
+            string p = "a.";
 
+            bool result = solution.IsMatch(s, p);
+
+            Console.WriteLine(result);
         }
     }
 }
---
+++ 11. Container With Most Water/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: height = [1,8,6,2,5,4,8,3,7]
+    Output: 49
+    Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+ * Example 2:
+    Input: height = [1,1]
+    Output: 1
+ */
