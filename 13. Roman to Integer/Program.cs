535a77d0bfecfc181e6251b6887e304c748e74e1
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-15 11:05:18 +0700

    Demine
    15/10/2023

--- 13. Roman to Integer/Program.cs
+++ 13. Roman to Integer/Program.cs
@@ -4,7 +4,33 @@
     {
         public int RomanToInt(string s)
         {
+            char[] romanNumerals = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+            int[] values = { 1, 5, 10, 50, 100, 500, 1000 };
+            int result = 0;
 
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentChar = s[i];
+                int currentValue = values[Array.IndexOf(romanNumerals, currentChar)];
+
+                int nextValue = 0;
+                if (i + 1 < s.Length)
+                {
+                    char nextChar = s[i + 1];
+                    nextValue = values[Array.IndexOf(romanNumerals, nextChar)];
+                }
+
+                if (currentValue < nextValue)
+                {
+                    result -= currentValue;
+                }
+                else
+                {
+                    result += currentValue;
+                }
+            }
+
+            return result;
         }
     }
 
@@ -14,7 +40,7 @@
         {
             Solution solution = new Solution();
 
-            string s = "";
+            string s = "MCMXCIV";
             int result = solution.RomanToInt(s);
 
             Console.WriteLine(result);
