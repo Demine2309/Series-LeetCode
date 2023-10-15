b8f851634df48059fef1602be7232c4023e5e73c
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-15 17:18:29 +0700

    Demine
    15/10/2023

---
+++ 15. 3Sum/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+
+namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            Array.Sort(nums); // Sort the array to make it easier to identify unique combinations.
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+                {
+                    int left = i + 1;
+                    int right = nums.Length - 1;
+                    int target = 0 - nums[i];
+
+                    while (left < right)
+                    {
+                        int sum = nums[left] + nums[right];
+
+                        if (sum == target)
+                        {
+                            result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                            // Skip duplicates to avoid duplicate combinations.
+                            while (left < right && nums[left] == nums[left + 1]) left++;
+                            while (left < right && nums[right] == nums[right - 1]) right--;
+
+                            left++;
+                            right--;
+                        }
+                        else if (sum < target)
+                        {
+                            left++;
+                        }
+                        else
+                        {
+                            right--;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public void Print(IList<IList<int>> result)
+        {
+            foreach (var combination in result)
+            {
+                Console.Write("[");
+                foreach (var element in combination)
+                {
+                    Console.Write(element);
+                    if (element != combination.Last())
+                    {
+                        Console.Write(",");
+                    }
+                }
+                Console.Write("] ");
+            }
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] array = { -1, 0, 1, 2, -1, -4 };
+
+            IList<IList<int>> result = solution.ThreeSum(array);
+
+            solution.Print(result);
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: nums = [-1,0,1,2,-1,-4]
+    Output: [[-1,-1,2],[-1,0,1]]
+    Explanation: 
+    nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+    nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+    nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+    The distinct triplets are [-1,0,1] and [-1,-1,2].
+    Notice that the order of the output and the order of the triplets does not matter.
+
+ * Example 2:
+    Input: nums = [0,1,1]
+    Output: []
+    Explanation: The only possible triplet does not sum up to 0.
+
+ * Example 3:
+    Input: nums = [0,0,0]
+    Output: [[0,0,0]]
+    Explanation: The only possible triplet sums up to 0.
+ */
--- Test everything/Program.cs
+++ Test everything/Program.cs
@@ -1,12 +1,73 @@
 ﻿class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        string s = "+2234";
-        int result;
+        int[] array = { -1, 0, 1, 2, -1, -4 };
 
-        result = Int32.Parse(s);
+        IList<IList<int>> result = GroupOf3(array);
 
-        Console.WriteLine(result);
+        Print(result);
+    }
+
+    public static IList<IList<int>> GroupOf3(int[] nums)
+    {
+        IList<IList<int>> result = new List<IList<int>>();
+
+        Array.Sort(nums); // Sort the array to make it easier to identify unique combinations.
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+            {
+                int left = i + 1;
+                int right = nums.Length - 1;
+                int target = 0 - nums[i];
+
+                while (left < right)
+                {
+                    int sum = nums[left] + nums[right];
+
+                    if (sum == target)
+                    {
+                        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                        // Skip duplicates to avoid duplicate combinations.
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    }
+                    else if (sum < target)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void Print(IList<IList<int>> result)
+    {
+        foreach (var combination in result)
+        {
+            Console.Write("[");
+            foreach (var element in combination)
+            {
+                Console.Write(element);
+                if (element != combination.Last())
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.Write("] ");
+        }
+
     }
 }
