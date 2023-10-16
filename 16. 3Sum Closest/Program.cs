64b8227a6f576504ea6f15f829c58c3ad189b97a
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-16 23:38:13 +0700

    Demine
    16/10/2023

---
+++ 16. 3Sum Closest/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: nums = [-1,2,1,-4], target = 1
+    Output: 2
+    Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+ * Example 2:
+    Input: nums = [0,0,0], target = 1
+    Output: 0
+    Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ */
