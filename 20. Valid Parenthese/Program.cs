d5addd77a83143feff85c1949ae8f543fff557fa
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-24 16:41:42 +0700

    Demine
    24/10/2023

---
+++ 20. Valid Parenthese/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' || c == ']' || c == '}')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false; 
+                    }
+
+                    char openingBracket = stack.Pop();
+                    if (!AreBracketsMatching(openingBracket, c))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0; 
+        }
+
+        private bool AreBracketsMatching(char opening, char closing)
+        {
+            return (opening == '(' && closing == ')') ||
+                   (opening == '[' && closing == ']') ||
+                   (opening == '{' && closing == '}');
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            string s = "(){}({)";
+
+            bool result = solution.IsValid(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: s = "()"
+    Output: true
+
+ * Example 2:
+    Input: s = "()[]{}"
+    Output: true
+ 
+ * Example 3:
+    Input: s = "(]"
+    Output: false
+*/
---
+++ 22. Generate parentheses/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public IList<string> GenerateParentheses(int n)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: n = 3
+    Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+ * Example 2:
+    Input: n = 1
+    Output: ["()"]
+*/
