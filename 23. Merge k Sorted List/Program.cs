50766db97fa89771b7029501ffc9e56291e0f7fb
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-29 11:15:01 +0700

    Demine
    29/10/2023

--- 23. Merge k Sorted List/Program.cs
+++ 23. Merge k Sorted List/Program.cs
@@ -17,8 +17,69 @@
     {
         public ListNode MergeKLists(ListNode[] lists)
         {
+            if (lists == null || lists.Length == 0)
+                return null;
 
+            int interval = 1;
+            while (interval < lists.Length)
+            {
+                for (int i = 0; i + interval < lists.Length; i += interval * 2)
+                {
+                    lists[i] = MergeTwoLists(lists[i], lists[i + interval]);
+                }
+
+                interval *= 2;
+            }
+
+            return lists[0];
         }
+
+        private ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode dummy = new ListNode();
+            ListNode tail = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    tail.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    tail.next = list2;
+                    list2 = list2.next;
+                }
+
+                tail = tail.next;
+            }
+
+            // Append the remaining elements of list 1 or list 2
+            if (list1 != null)
+            {
+                tail.next = list1;
+            }
+            else
+            {
+                tail.next = list2;
+            }
+
+            return dummy.next;
+        }
+
+        public void PrintLinkedList(ListNode head)
+        {
+            ListNode current = head;
+
+            while (current != null)
+            {
+                Console.Write(current.val + " -> ");
+                current = current.next;
+            }
+            Console.WriteLine("null");
+        }
+
     }
 
     class MainClass
@@ -27,7 +88,16 @@
         {
             Solution solution = new Solution();
 
+            ListNode[] lists = new ListNode[]
+            {
+                new ListNode(1, new ListNode (2, new ListNode (5))),
+                new ListNode(0, new ListNode (1, new ListNode (4))),
+                new ListNode(2, new ListNode(6))
+            };
 
+            ListNode result = solution.MergeKLists(lists);
+
+            solution.PrintLinkedList(result);
         }
     }
 }
---
+++ 24. Swap nodes in Pairs/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SeriesLeetCode
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: head = [1,2,3,4]
+    Output: [2,1,4,3]
+
+ * Example 2:
+    Input: head = []
+    Output: []
+
+ * Example 3:
+    Input: head = [1]
+    Output: [1]
+ */
