3a61769c4f592b20e9d2577ed93866072cda91bf
Demine Nguyen <cuonghustmi1@gmail.com>
2023-11-01 09:13:42 +0700

    Demine
    1/11/2023

--- 24. Swap nodes in Pairs/Program.cs
+++ 24. Swap nodes in Pairs/Program.cs
@@ -15,9 +15,57 @@
 
     public class Solution
     {
+        #region Recursive Approach
+        //public ListNode SwapPairs(ListNode head)
+        //{
+        //    if (head == null || head.next == null)
+        //        return head;
+
+        //    ListNode newHead = head.next;
+
+        //    head.next = SwapPairs(head.next.next);
+        //    newHead.next = head;
+
+        //    return newHead;
+        //}
+        #endregion
+
+        #region Iterative Approach
         public ListNode SwapPairs(ListNode head)
         {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
 
+            ListNode prev = dummy;
+            ListNode cur = head;
+
+            while(cur != null && cur.next != null)
+            {
+                ListNode firstNode = cur;
+                ListNode secondNode = cur.next;
+
+                prev.next = secondNode;
+                firstNode.next = secondNode.next;
+                secondNode.next = firstNode;
+
+                prev = firstNode;
+                cur = firstNode.next;
+            }
+
+            return dummy.next;
+        }
+        #endregion
+
+        public void PrintLinkedList(ListNode head)
+        {
+            ListNode current = head;
+
+            while (current != null)
+            {
+                Console.Write(current.val + " -> ");
+                current = current.next;
+            }
+            Console.WriteLine("null");
         }
     }
 
@@ -27,6 +75,11 @@
         {
             Solution solution = new Solution();
 
+            ListNode head = new ListNode(1, new ListNode(2, new ListNode(5, new ListNode(4))));
+
+            ListNode result = solution.SwapPairs(head);
+
+            solution.PrintLinkedList(result);
         }
     }
 }
