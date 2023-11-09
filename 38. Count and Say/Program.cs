dcd6b91895b1ebfcdcbc418a7bc17b31ce11ef0a
Cuong <cuonghustmi1@gmail.com>
2023-11-09 09:47:38 +0700

    Demine
    9/11/2023

--- 38. Count and Say/Program.cs
+++ 38. Count and Say/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public string CountAndSay(int n)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: n = 1
+    Output: "1"
+    Explanation: This is the base case.
+
+ * Example 2:
+    Input: n = 4
+    Output: "1211"
+    Explanation:
+    countAndSay(1) = "1"
+    countAndSay(2) = say "1" = one 1 = "11"
+    countAndSay(3) = say "11" = two 1's = "21"
+    countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ */
