17ba0bd9625168a63b81612bfadbc25a0d9c8914
Cuong <cuonghustmi1@gmail.com>
2023-11-10 15:43:47 +0700

    Demine
    10/11/2023

--- 39. Combination Sum/Program.cs
+++ 39. Combination Sum/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace SeriesLeetCode
+﻿using System.Collections.Generic;
+
+namespace SeriesLeetCode
 {
     public class Solution
     {
         public IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
+            IList<IList<int>> result = new List<IList<int>>();
+            IList<int> current = new List<int>();
 
+            BackTrack(candidates, target, 0, current, result);
+
+            return result;
+        }
+
+        private void BackTrack(int[] arr, int target, int start, IList<int> current, IList<IList<int>> result)
+        {
+            if (target == 0)
+            {
+                result.Add(new List<int>(current));
+                return;
+            }
+
+            if (target < 0)
+            {
+                return;
+            }
+
+            for (int i = start; i < arr.Length; i++)
+            {
+                current.Add(arr[i]);
+                BackTrack(arr, target - arr[i], i, current, result);
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+
+        public void Print(IList<IList<int>> result)
+        {
+            foreach (var combination in result)
+            {
+                Console.Write("[");
+                foreach (var element in combination)
+                {
+                    Console.Write(element);
+                    if (element != combination.Last())
+                    {
+                        Console.Write(",");
+                    }
+                }
+                Console.Write("] ");
+            }
         }
     }
 
@@ -14,7 +59,12 @@
         {
             Solution solution = new Solution();
 
+            int[] candidates = { 2, 3, 5 , 7};
+            int target = 7;
 
+            IList<IList<int>> result = solution.CombinationSum(candidates, target);
+
+            solution.Print(result);
         }
     }
 }
