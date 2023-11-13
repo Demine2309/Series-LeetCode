4f5a3e67bd0d8c6e6c5c6c5bea36a2fbcd1dc095
Cuong <cuonghustmi1@gmail.com>
2023-11-13 18:47:06 +0700

    Demine
    13/11/2023

---
+++ 40. Combination Sum II/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+        {
+
+        }
+    }
+
+    class MainCLass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: candidates = [10,1,2,7,6,1,5], target = 8
+    Output: 
+    [
+    [1,1,6],
+    [1,2,5],
+    [1,7],
+    [2,6]
+    ]
+
+ * Example 2:
+    Input: candidates = [2,5,2,1,2], target = 5
+    Output: 
+    [
+    [1,2,2],
+    [5]
+    ]
+ */
