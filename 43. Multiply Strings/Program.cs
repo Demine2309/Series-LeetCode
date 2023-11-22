d299fe9f5934eed4610321d7df2c8082f0ec4105
Cuong <cuonghustmi1@gmail.com>
2023-11-22 09:20:13 +0700

    Demine
    22/11/2023

--- 43. Multiply Strings/Program.cs
+++ 43. Multiply Strings/Program.cs
@@ -1,20 +1,59 @@
-﻿namespace SeriesLeetCode
+﻿using System.Text;
+
+namespace SeriesLeetCode
 {
     public class Solution
     {
         public string Multiply(string num1, string num2)
         {
+            if (num1 == "0" || num2 == "0")
+                return "0";
+
+
+            int[] result = new int[num1.Length + num2.Length];
+
+            for (int i = num1.Length - 1; i >= 0; i--)
+            {
+                for (int j = num2.Length - 1; j >= 0; j--)
+                {
+                    int digit1 = num1[i] - '0';
+                    int digit2 = num2[j] - '0';
+                    int product = digit1 * digit2;
+
+                    int pos1 = i + j;
+                    int pos2 = i + j + 1;
+                    int total = result[pos2] + product;
+
+                    result[pos2] = total % 10;
+                    result[pos1] += total / 10;
+                }
+            }
+
+            int start = 0;
+            while (start < result.Length && result[start] == 0)
+                start++;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = start; i < result.Length; i++)
+                sb.Append(result[i]);
+
+            return sb.ToString();
 
         }
     }
 
-    class MainClass
+    class MainClass 
     {
         static void Main(string[] args)
         {
             Solution solution = new Solution();
 
+            string num1 = "123";
+            string num2 = "456";
 
+            string result = solution.Multiply(num1, num2);
+
+            Console.WriteLine(result);
         }
     }
 }
---
+++ 45. Jump Game II/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public int Jump(int[] nums)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: nums = [2,3,1,1,4]
+    Output: 2
+    Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+ * Example 2:
+    Input: nums = [2,3,0,1,4]
+    Output: 2
+ */
