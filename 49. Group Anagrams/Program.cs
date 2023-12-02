bae6364c421cdeb5a35c40ed234f3f2cc6975303
Cuong <cuonghustmi1@gmail.com>
2023-12-02 12:38:49 +0700

    Demine
    2/12/2023

--- 49. Group Anagrams/Program.cs
+++ 49. Group Anagrams/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace SeriesLeetCode
+﻿using System.Collections.Immutable;
+
+namespace SeriesLeetCode
 {
     public class Solution
     {
-        public IList<IList<int>> GroupAnagrams(string[] strs)
+        public IList<IList<string>> GroupAnagrams(string[] strs)
         {
+            IList<IList<string>> result = new List<IList<string>>();
+            IList<string> element = new List<string>();
+            IList<string> group = new List<string>();
 
+            for (int i = 0; i < strs.Length;i++)
+            {
+                char[] c = strs[i].ToCharArray();
+
+                Array.Sort(c);
+
+                
+                group.Add(c);
+            }
+        }
+
+        public void Print(IList<IList<string>> result)
+        {
+            foreach (var combination in result)
+            {
+                Console.Write("[");
+                foreach (var element in combination)
+                {
+                    Console.Write(element);
+                    if (element != combination.Last())
+                    {
+                        Console.Write(",");
+                    }
+                }
+                Console.Write("] ");
+            }
         }
     }
 
@@ -12,8 +43,10 @@
     {
         static void Main(string[] args)
         {
-            Solution solution = new Solution(); 
+            Solution solution = new Solution();
+            string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
 
+            IList<IList<string>> result = solution.GroupAnagrams(strs);
 
         }
     }
