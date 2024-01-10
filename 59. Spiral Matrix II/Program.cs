d5fce6f1346b46630938cd0eeed280f606d15403
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-01-10 23:10:46 +0700

    Demine
    10/1/2024

---
+++ 59. Spiral Matrix II/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public int[][] GenerateMatrix(int n)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: n = 3
+    Output: [[1,2,3],[8,9,4],[7,6,5]]
+
+ * Example 2:
+    Input: n = 1
+    Output: [[1]]
+ */
