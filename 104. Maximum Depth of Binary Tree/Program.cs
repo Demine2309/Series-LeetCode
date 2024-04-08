0120db386ee604794c0c36542303f225f3fed604
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-08 10:32:25 +0700

    Demine
    08/04/2024

--- 104. Maximum Depth of Binary Tree/Program.cs
+++ 104. Maximum Depth of Binary Tree/Program.cs
@@ -19,7 +19,13 @@
 
         public int MaxDepth(TreeNode root)
         {
+            if(root == null) return 0;
 
+            int leftDepth = MaxDepth(root.left);
+
+            int rightDepth = MaxDepth(root.right);
+
+            return Math.Max(leftDepth, rightDepth) + 1;
         }
     }
 
---
+++ 108. Convert Sorted Array to Binary Seach Tree/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SeriesLeetCode
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left, right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public TreeNode root;
+
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+
+        }
+
+        public void Inorder(TreeNode root, int[] nums)
+        {
+            if(root == null) return;
+
+            Inorder(root.left, nums);
+            Console.Write(root.val);
+            Inorder(root.right, nums);
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution tree = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: nums = [-10,-3,0,5,9]
+    Output: [0,-3,9,-10,null,5]
+    Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+ * Example 2:
+    Input: nums = [1,3]
+    Output: [3,1]
+    Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ */
