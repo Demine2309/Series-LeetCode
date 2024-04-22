ec824c3eaef77c8c4de08ddb5bfd112aa344d0c5
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-04-22 12:44:48 +0700

    Demine
    22/04/2024

---
+++ 118. Pascal's Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SeriesLeetCode
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+        }
+    }
+}
+
+/*
+ * Example 1:
+    Input: numRows = 5
+    Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+ * Example 2:
+    Input: numRows = 1
+    Output: [[1]]
+ */
